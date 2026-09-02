6957cb73ad2d9247a5853de535cb109cfaba4bfa
Thanh Tai <taidoannguyenthanh20@gmail.com>
2026-09-03 05:18:40 +0700

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,1 @@
-function Contact() {
-  return (
-    <div className="page">
-      <h1>TODO: Liên hệ</h1>
 
-      <p>
-        Hãy tự thiết kế trang liên hệ.
-      </p>
-
-      {/* TODO: Tạo form liên hệ */}
-    </div>
-  );
-}
-
-export default Contact;
