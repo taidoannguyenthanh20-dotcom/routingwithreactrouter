57f70c2fc12aa8661df1d53eb89cf3736e389ced
Thanh Tai <taidoannguyenthanh20@gmail.com>
2026-09-03 05:18:29 +0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,1 @@
-function About() {
-  return (
-    <div className="page">
-      <h1>TODO: Giới thiệu</h1>
 
-      <p>
-        Hãy tự thiết kế trang giới thiệu.
-      </p>
-
-      {/* TODO: Thêm thông tin cá nhân */}
-    </div>
-  );
-}
-
-export default About;
