f25894890f5930f3230aae3f6b8d347c5699907f
Thanh Tai <taidoannguyenthanh20@gmail.com>
2026-09-03 05:18:48 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,1 @@
-function Home() {
-  return (
-    <div className="page">
-      <h1>TODO: Trang chủ</h1>
 
-      <p>
-        Hãy tự thiết kế nội dung cho trang chủ.
-      </p>
-
-      {/* TODO: Thêm nội dung của bạn */}
-    </div>
-  );
-}
-
-export default Home;
